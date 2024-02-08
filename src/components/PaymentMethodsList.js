df9aef977bd70e2abd41996bb19e1330603e2d9d
Oscar Abraham <oscara1706cl@gmail.com>
2024-02-08 08:35:47 -0600

    added functionality to set default payment method

--- src/components/PaymentMethodsList.js
+++ src/components/PaymentMethodsList.js
@@ -14,7 +14,7 @@ import { showErrorNotification } from '../services/notificationService'
 const PaymentMethodsList = () => {
     const [paymentMethods, setPaymentMethods] = useState({ default_payment_method: null, all_payment_methods: [] })
     const [fetchPaymentMethods, setFetchPaymentMethods] = useState(false);
-    const [paymentMethodToUpdate, setpaymentMethodToUpdate] = useState(null);
+    const [defaultPaymentMethod, setDefaultPaymentMethod] = useState(null);
     const [showForm, setShowForm] = useState(false);
 
     const getPaymentMethods = async () => {
@@ -31,6 +31,7 @@ const PaymentMethodsList = () => {
               }, {timeout:5000})
               console.log(response.data);
               setPaymentMethods(response.data)
+              setDefaultPaymentMethod(response.data.default_payment_method)
         } catch(error){
             showErrorNotification(error);
             console.log(error);
@@ -61,7 +62,7 @@ const PaymentMethodsList = () => {
 
     const deletePaymentMethod = async (methodId) => {
         console.log(methodId);
-        setpaymentMethodToUpdate(null);
+       
         setShowForm(false)
         let user = getUser();
         // Adjusted URL to include methodId if your API requires it in the URL
@@ -84,16 +85,31 @@ const PaymentMethodsList = () => {
         }
       };
 
-      const setDefaultPaymentMethod = async (method) => {
-        // Placeholder for update logic
-        console.log(`Updating payment method with ID: ${method}`);
-        // You would typically show a form to collect new payment details here
-        // and then send a PUT or PATCH request to your backend
-      };
+      const applyDefaultPaymentMethod = async (method) => {
+        console.log(`Updating payment method with ID: ${method.id}`);
+        let user = getUser();
+        let url = `${process.env.REACT_APP_BASE_URL}/api/payment_method/${user.id}/`;
+        const token = getAccessToken();
+        const data = { payment_method_id: method.id }; // Data payload to be sent in the request body
+    
+        const config = {
+            headers: { Authorization: `Bearer ${token}` },
+            timeout: 5000,
+        };
+    
+        try {
+            await axios.put(url, data, config); // Note the corrected order: URL, data, config
+            console.log(`Set default payment method with ID: ${method.id}`);
+            setFetchPaymentMethods(prevState => !prevState); // Trigger re-fetch of payment methods
+        } catch (error) {
+            showErrorNotification(error);
+            console.error(error);
+        }
+    };
 
       const addMethodsButton = () =>{
         setShowForm(!showForm)
-        setpaymentMethodToUpdate(null)
+        
       }
       
       
@@ -104,14 +120,14 @@ const PaymentMethodsList = () => {
     return (
         <div>
             <h4>Metodos de pago</h4>
-            <div class="instructions-container">
+            <div className="instructions-container">
                 <h5>Instrucciones para la Gestión de Métodos de Pago</h5>
                 <p>Añade, configura y elimina métodos de pago fácilmente desde tu perfil. Selecciona tu metodo principal para transacciones futuras. Gestiona tus pagos con seguridad y conveniencia.</p>
             </div>
             {paymentMethods.all_payment_methods.length > 0 && (
                 <div className="payment-methods-list">
                     {paymentMethods.all_payment_methods.map((method) => (
-                        <div key={method.id} className={`payment-method-box  ${paymentMethodToUpdate && paymentMethodToUpdate.id === method.id ? 'payment-method-selected' : ''}`}>
+                        <div key={method.id} className={`payment-method-box  ${defaultPaymentMethod && defaultPaymentMethod.id === method.id ? 'payment-method-selected' : ''}`}>
                             <div className="payment-method-details">
                                 <FontAwesomeIcon icon={getCardBrandIcon(method.card.brand)} />
                                 <p>Brand: {method.card.brand.toUpperCase()}</p>
@@ -120,8 +136,14 @@ const PaymentMethodsList = () => {
                                 {/* Add more details as needed */}
                             </div>
                             <div className="payment-method-actions">
-                            
-                            <Button variant="secondary" size="sm" onClick={() => setDefaultPaymentMethod(method)}>Principal metodo de Pago</Button>
+
+                            {
+                                defaultPaymentMethod && defaultPaymentMethod.id === method.id  
+                                    ?
+                                <span>*Pago Principal</span> 
+                                    :
+                                <Button variant="secondary" size="sm" onClick={() => applyDefaultPaymentMethod(method)}>Principal metodo de Pago</Button>
+                            }    
                             
                             <Button variant="danger" size="sm" onClick={() => deletePaymentMethod(method.id)}>Eliminar</Button>
                             </div>
