d94ec6b1a6f87c64986284cc5a1dde97e60c2280
Oscar Carrillo <oscar.carrillo2941@alumnos.udg.mx>
2024-06-20 20:57:43 -0600

    fixing commits

--- src/components/TermsOfService.js
+++ src/components/TermsOfService.js
@@ -4,6 +4,7 @@ import './TermsOfService.css'
 const termsSections = [
   {
     title: 'Acuerdo Legal',
+<<<<<<< HEAD
     content: `
 Le informamos que es importante y obligatorio que, en su carácter de Usuario y/o Visitante, en adelante el “USUARIO”, lea, entienda y acepte de manera total el contenido de este documento, dicho consentimiento se considera como otorgado a favor de SANDRA LOPEZ GONZALEZ, PERSONA FÍSICA CON ACTIVIDAD EMPRESARIAL, en adelante “Sat Nam Yoga Estudio®”.
 Al consultar, registrarse, solicitar y/o usar los servicios (aun gratuitamente), denominado en lo subsecuente como el “SERVICIO”, que se presentan a través del sitio web 
@@ -24,6 +25,31 @@ el Usuario puede utilizar los mecanismos establecidos en la Ley ante la Procurad
 Si usted es un Usuario no establecido en México, deberá consultar los Términos y Condiciones que Sat Nam Yoga Estudio®  pone a disposición para Usuarios que radiquen en países distintos a México.
 
     .`,
+=======
+    content: `Le informamos que es importante y obligatorio 
+    que, en su carácter de Usuario y/o Visitante, en adelante el “USUARIO”, 
+    lea, entienda y acepte de manera total el contenido de este documento, dicho consentimiento se considera como otorgado a favor de SANDRA LOPEZ GONZALEZ, PERSONA FÍSICA CON ACTIVIDAD EMPRESARIAL,
+    en adelante “Sat Nam Yoga Estudio®”.
+
+    Al consultar, registrarse, solicitar y/o usar los servicios (aun gratuitamente), denominado en lo subsecuente como el “SERVICIO”, que se presentan a través del sitio web 
+    www.satnamyogaestudio.com, denominados en lo subsecuente como “LA PLATAFORMA”, implica la adhesión a estos Términos y Condiciones de Uso y del Servicio.
+
+    La importancia de que el Usuario conozca el contenido de estos términos y condiciones radica en que estos disponen las reglas establecidas por Sat Nam yoga Estudio®, 
+    para el uso de LA PLATAFORMA en los Estados Unidos Mexicanos, así como los derechos y obligaciones que tiene el Usuario como consumidor, 
+    salvo las excepciones previstas en diversas cláusulas.
+
+    El marco jurídico que en los Estados Unidos Mexicanos protege los derechos y obligaciones tanto de Sat Nam Yoga Estudio® como del Usuario se rigen por la legislación mexicana, específicamente 
+    la Ley Federal del Protección al Consumidor, el Código de Comercio, el Código Civil Federal, el Código Federal de Procedimientos Civiles y la Norma Mexicana ‘NMX-COE-001-SCFI-2018, 
+    Comercio Electrónico - Disposiciones a las que se sujetarán aquellas personas que ofrezcan, comercialicen o vendan bienes, productos o servicios’, la legislación mexicana señalada en 
+    su conjunto reglamenta el uso de servicios en línea para establecer tanto disposiciones normativas como sanciones en caso de que se realice la violación a los derechos de los 
+    consumidores en línea, por lo cual si usted considera que sus derechos como Usuario no están siendo protegidos bajo dicho marco legal, deberá conciliar inicialmente las 
+    quejas y reclamaciones del SERVICIO con Sat Nam Yoga Estudio® utilizando los medios que indican en estos términos y condiciones y, en caso de no obtener una respuesta satisfactoria, 
+    el Usuario puede utilizar los mecanismos establecidos en la Ley ante la Procuraduría Federal del Consumidor.
+
+    Si usted es un Usuario no establecido en México, deberá consultar los Términos y Condiciones que Sat Nam Yoga Estudio pone a disposición para Usuarios que radiquen en países distintos a México.
+
+    .`
+>>>>>>> d3c1cb1 (creating terms of service)
   },
   {
     title: 'CLÁUSULA A. IDENTIFICACIÓN DEL PRESTADOR DEL SERVICIO',
@@ -492,6 +518,7 @@ const TermsOfService = () => {
   }
 
   return (
+<<<<<<< HEAD
     <div className='termsOfService'>
       <h1>Términos de Servicio de Sat Nam Yoga Estudio.com</h1>
       <h4>
@@ -501,6 +528,15 @@ const TermsOfService = () => {
         GONZALEZ, PERSONA FÍSICA CON ACTIVIDAD EMPRESARIAL LOCALIZADA EN EL
         SITIO WEB &quot;WWW.SATNAMYOGAESTUDIO®.COM&quot; ACUERDO LEGAL
       </h4>
+=======
+    <div className="termsOfService">
+      <h1>Términos de Servicio de Sat Nam Yoga Estudio.com</h1>
+      <h2>LOS TÉRMINOS Y CONDICIONES DE USO Y DEL SERVICIO APLICABLES A LOS ESTADOS UNIDOS MEXICANOS, QUE DETERMINAN LA FORMA 
+        EN QUE SE REALIZA EL USO DE LA PLATAFORMA DE CURSOS EN LÍNEA QUE OFRECE SANDRA LOPEZ GONZALEZ, PERSONA FÍSICA CON ACTIVIDAD EMPRESARIAL LOCALIZADA EN EL SITIO 
+        WEB "WWW.SATNAMYOGAESTUDIO®.COM ”
+        ACUERDO LEGAL
+      </h2>
+>>>>>>> d3c1cb1 (creating terms of service)
       {termsSections.map((section, index) => (
         <div key={index} className='termsSection'>
           <h3 onClick={() => toggleSection(index)}>{section.title}</h3>
